8245a68d8eacbf0edefae4c57c006c20c5431741
Ishank Dua <ishank@github.com>
2020-08-06 14:11:56 +0530

    onCreateAdded on New Activity

--- app/src/main/java/com/gfg/www/toast/NewActivity.java
+++ app/src/main/java/com/gfg/www/toast/NewActivity.java
@@ -3,11 +3,15 @@ package com.gfg.www.toast;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class NewActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tva_main);
+
+
+        Toast.makeText(this, "HI there i am toast", Toast.LENGTH_SHORT).show();
     }
 }
